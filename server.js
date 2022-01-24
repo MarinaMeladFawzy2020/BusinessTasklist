1d34b302d28dd3bc53792899331be8c5cb3b73d7
Marina Melad <68344244+MarinaMeladFawzy2020@users.noreply.github.com>
2022-01-24 17:07:14 +0200

    serve

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const ngApp = express();
-ngApp.use(express.static('./dist/task-list'));
+ngApp.use(express.static('./dist/TaskList'));
 
 ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/task-list/index.html'));
+    response.sendFile(path.join(__dirname, '/dist/TaskList/index.html'));
 });
 ngApp.listen(process.env.PORT || 8080);
 
